4940151524f07b7d3474f8f79f29b805023bfe10
Anushiny Cedrick <anushinyc@gmail.com>
2024-03-29 13:14:59 -0700

    Index changes make

---
+++ index.js
@@ -0,0 +1,79 @@
+
+
+// const main = (a,b) =>{
+//     let nam = "cedrick"
+//     console.log(a+b)
+//     console.log(nam)
+// }
+
+// main(3,9)
+
+
+// const example =()=>{
+//     if(true){
+//         let myName = "Anushiny"
+//         console.log(myName)
+//     }
+    // console.log(myName)
+// }
+
+// example()
+// console.log(myName)
+
+
+// data type 
+// premtive data types 
+// string , number 
+
+// --------------Strings ------------
+
+
+// let firsName = "Anushiny "
+// let LastName = "Cedrick"
+
+// // -------Number ---------
+
+// let num = 1000.45
+// let x = 10
+// console.log(typeof firsName)
+// console.log(typeof x)
+
+
+// let x_1 =  30 < 10
+
+// console.log(typeof x_1)
+
+
+// let x_3=79;
+// console.log(x_3)
+// console.log(typeof x_3)
+
+
+// let ced = null
+// console.log(ced)
+// console.log(typeof ced)
+
+
+// console.log(null == undefined)
+
+
+// ----- reference data type object------
+
+let person = {
+    name: "Cedrick",
+    last: "Anushiny",
+    age: 12
+}
+
+console.log(person)
+console.log(typeof person)
+
+// ---- Array types of object which store a collection of values--
+let num_1= [1,2,3,4,5]
+console.log(num_1)
+console.log(typeof num_1)
+
+
+// ---function are type of object which is used to execute code"
+
+
